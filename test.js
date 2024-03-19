5519fe2add7154b5bc1d4ceacbb9ab96c33459e0
zhangjialiang <zhangjialiang2007@163.com>
2024-03-19 10:30:13 +0800

    use es format

--- test.js
+++ test.js
@@ -89,39 +89,4 @@ class timeDependentDijkstra {
     }
 }
 
-// 示例路网数据  
-const graph = {
-    A: {
-        B: new Edge(new Node('A'), new Node('B'), (departureTime) => 10),
-        C: new Edge(new Node('A'), new Node('C'), (departureTime) => 15),
-    },
-    B: {
-        D: new Edge(new Node('B'), new Node('D'), (departureTime) => {
-            if (departureTime < 10) {
-                return 10;
-            } else {
-                return 15;
-            }
-        }),
-    },
-    C: {
-        E: new Edge(new Node('C'), new Node('E'), (departureTime) => {
-            if (departureTime < 20) {
-                return 1; // 假设上午12点前需要10分钟  
-            } else {
-                return 15; // 假设12点后需要15分钟  
-            }
-        }),
-    },
-    D: {
-        F: new Edge(new Node('D'), new Node('F'), (departureTime) => 5),
-    },
-    E: {
-        F: new Edge(new Node('E'), new Node('F'), (departureTime) => 5),
-    }
-};
-
-const startNode = new Node('A');
-let a = new timeDependentDijkstra(graph, startNode);
-console.log('A-F的最短路径',a.getShortestPath('A','F'))
-console.log('A-E的最短路径',a.getShortestPath('A','E'))
+export {Node, Edge, timeDependentDijkstra}
